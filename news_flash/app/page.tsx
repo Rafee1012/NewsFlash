966faa9a4971d41d37ca0f6e92491ee5266bfbdd
rafee01 <rafee01@students.cs.ubc.ca>
2026-05-09 13:25:39 -0700

    Initial commit

---
+++ news_flash/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className = "text-2xl underline">
+            Welcome to my website
+        </div>
+    )
+}
+
+export default Home
