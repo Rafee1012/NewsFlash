ef839238b1d56d2f08dbf6e4bcb0eaca40c380a7
rafee01 <rafee01@students.cs.ubc.ca>
2026-05-10 10:30:06 -0700

    Added route groups

---
+++ news_flash/app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+const Layout = ({children}: {children: React.ReactNode}) => {
+    return (
+        <div>
+            <p>Layout</p>
+            {children}
+        </div>
+    )
+}
+
+export default Layout
