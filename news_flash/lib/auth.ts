bf1c7d23bb89d40cb2d7cc17e9d1e90a3974c41f
rafee01 <rafee01@students.cs.ubc.ca>
2026-05-14 21:33:02 -0700

    Installed better-auth framework

--- news_flash/lib/auth.ts
+++ news_flash/lib/auth.ts
@@ -1,10 +1,6 @@
 import { betterAuth } from "better-auth";
 import Database from "better-sqlite3";
 
-const database = new Database("auth.db");
-
 export const auth = betterAuth({
-  database: database,
-  baseURL: "http://localhost:3000/",
-  emailAndPassword: { enabled: true },
-});
+    database: new Database("./sqlite.db"),
+})
